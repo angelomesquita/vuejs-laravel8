c336d7bef2d66370f39d966e281aafae5dbdfeec
Angelo Mesquita <angeloamesquita@gmail.com>
2021-05-03 17:05:39 -0300

    Versionando no github

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'http://localhost:8000/'
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+store.dispatch('auth/me').then(() => {
+    new Vue({
+        router,
+        store,
+        render: h => h(App)
+    }).$mount('#app')
+})
